bb868534e748f43e21d2d7139886003499f2f203
Ashutosh Chapagain <itsmeashutosh43@gmail.com>
2018-01-10 13:59:52 +0545

    Just Added location service

--- app/src/main/java/com/example/ashutosh/okhati/Dashboard.java
+++ app/src/main/java/com/example/ashutosh/okhati/Dashboard.java
@@ -1,13 +1,28 @@
 package com.example.ashutosh.okhati;
 
+import android.*;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class Dashboard extends AppCompatActivity {
+
+    int REQ=1;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public void buildAlert()
     {
@@ -36,5 +51,90 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        mFusedLocationClient= LocationServices.getFusedLocationProviderClient(this);
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 1:{
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    int check= ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+                    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if (location!=null)
+                            {
+                                Log.i("longitude",location.getLongitude()+"");
+                            }
+
+                            Log.i("ahha","lol");
+
+
+
+                        }
+                    });
+
+
+
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+
+            }
+
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int check= ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (check== PackageManager.PERMISSION_GRANTED)
+        {
+
+            Log.i("ahha","lol");
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+
+                    if (location!=null)
+                    {
+                        CharSequence aa=(CharSequence)(location.getLongitude()+"");
+
+                        Toast.makeText(Dashboard.this,aa,Toast.LENGTH_LONG).show();
+
+
+
+                        Log.i("longitude",location.getLongitude()+"");
+                    }
+
+                    Log.i("ahha","lol");
+
+
+
+                }
+            });
+
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQ);
+        }
+    }
+
 }
